d8dfbe36d5468bac143d713f5b9fcf6eee6ca702
Jakub <jakubfrolow@gmail.com>
2025-10-28 19:56:05 +0100

    Initial commit

---
+++ src/main/java/com/project/authapi/system_log_analyzer/SystemLogAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.project.authapi.system_log_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemLogAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemLogAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/authapi/system_log_analyzer/SystemLogAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.authapi.system_log_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemLogAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
